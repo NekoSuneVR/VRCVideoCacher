691fe0ac6038922807f3d9cd4943dab673c14481
NekoSuneVR <chisdeal2014@hotmail.com>
2026-01-03 22:28:04 +0000

    aaa

--- VRCVideoCacher.Server/API/APIController.cs
+++ VRCVideoCacher.Server/API/APIController.cs
@@ -14,136 +14,154 @@ public class ApiController : WebApiController
     [Route(HttpVerbs.Post, "/youtube-cookies")]
     public async Task ReceiveYoutubeCookies()
     {
-        using var reader = new StreamReader(HttpContext.OpenRequestStream(), Encoding.UTF8);
-        var cookies = await reader.ReadToEndAsync();
-        if (!Program.IsCookiesValid(cookies))
+        try
         {
-            Log.Error("Invalid cookies received, maybe you haven't logged in yet, not saving.");
-            HttpContext.Response.StatusCode = 400;
-            await HttpContext.SendStringAsync("Invalid cookies.", "text/plain", Encoding.UTF8);
-            return;
+            using var reader = new StreamReader(HttpContext.OpenRequestStream(), Encoding.UTF8);
+            var cookies = await reader.ReadToEndAsync();
+            if (!Program.IsCookiesValid(cookies))
+            {
+                Log.Error("Invalid cookies received, maybe you haven't logged in yet, not saving.");
+                HttpContext.Response.StatusCode = 400;
+                await HttpContext.SendStringAsync("Invalid cookies.", "text/plain", Encoding.UTF8);
+                return;
+            }
+
+            await File.WriteAllTextAsync(YtdlManager.CookiesPath, cookies);
+
+            HttpContext.Response.StatusCode = 200;
+            await HttpContext.SendStringAsync("Cookies received.", "text/plain", Encoding.UTF8);
+
+            Log.Information("Received Youtube cookies from browser extension.");
+            if (!ConfigManager.Config.ytdlUseCookies) 
+                Log.Warning("Config is NOT set to use cookies from browser extension.");
         }
-
-        await File.WriteAllTextAsync(YtdlManager.CookiesPath, cookies);
-
-        HttpContext.Response.StatusCode = 200;
-        await HttpContext.SendStringAsync("Cookies received.", "text/plain", Encoding.UTF8);
-
-        Log.Information("Received Youtube cookies from browser extension.");
-        if (!ConfigManager.Config.ytdlUseCookies) 
-            Log.Warning("Config is NOT set to use cookies from browser extension.");
+        catch (Exception ex)
+        {
+            Log.Error(ex, "ReceiveYoutubeCookies failed.");
+            HttpContext.Response.StatusCode = 500;
+            await HttpContext.SendStringAsync("Internal server error.", "text/plain", Encoding.UTF8);
+        }
     }
 
     [Route(HttpVerbs.Get, "/getvideo")]
     public async Task GetVideo()
     {
-        // escape double quotes for our own safety
-        var requestUrl = Request.QueryString["url"]?.Replace("\"", "%22").Trim();
-        var avPro = string.Compare(Request.QueryString["avpro"], "true", StringComparison.OrdinalIgnoreCase) == 0;
-        var source = Request.QueryString["source"] ?? "vrchat";
+        try
+        {
+            // escape double quotes for our own safety
+            var requestUrl = Request.QueryString["url"]?.Replace("\"", "%22").Trim();
+            var avPro = string.Compare(Request.QueryString["avpro"], "true", StringComparison.OrdinalIgnoreCase) == 0;
+            var source = Request.QueryString["source"] ?? "vrchat";
         
-        if (string.IsNullOrEmpty(requestUrl))
-        {
-            Log.Error("No URL provided.");
-            await HttpContext.SendStringAsync("No URL provided.", "text/plain", Encoding.UTF8);
-            return;
-        }
-
-        Log.Information("Request URL: {URL}", requestUrl);
-
-        if (requestUrl.StartsWith("https://dmn.moe"))
-        {
-            requestUrl = requestUrl.Replace("/sr/", "/yt/");
-            Log.Information("YTS URL detected, modified to: {URL}", requestUrl);
-        }
-
-        if (ConfigManager.Config.BlockedUrls.Any(blockedUrl => requestUrl.StartsWith(blockedUrl)))
-        {
-            Log.Warning("URL Is Blocked: {url}", requestUrl);
-            requestUrl = ConfigManager.Config.BlockRedirect;
-        }
-
-        var videoInfo = await VideoId.GetVideoId(requestUrl, avPro);
-        if (videoInfo == null)
-        {
-            Log.Information("Failed to get Video Info for URL: {URL}", requestUrl);
-            return;
-        }
-
-        var (isCached, filePath, fileName) = GetCachedFile(videoInfo.VideoId, avPro);
-        if (isCached)
-        {
-            File.SetLastWriteTimeUtc(filePath, DateTime.UtcNow);
-            CacheManager.AddToCache(fileName);
-            var url = $"{ConfigManager.Config.ytdlWebServerURL}/{fileName}";
-            Log.Information("Responding with Cached URL: {URL}", url);
-            await HttpContext.SendStringAsync(url, "text/plain", Encoding.UTF8);
-            return;
-        }
-
-        if (string.IsNullOrEmpty(videoInfo.VideoId))
-        {
-            Log.Information("Failed to get Video ID: Bypassing.");
-            await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
-            return;
-        }
-
-        if (requestUrl.StartsWith("https://mightygymcdn.nyc3.cdn.digitaloceanspaces.com"))
-        {
-            Log.Information("URL Is Mighty Gym: Bypassing.");
-            await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
-            return;
-        }
-
-        if (source == "resonite")
-        {
-            Log.Information("Request sent from resonite sending json.");
-            await HttpContext.SendStringAsync(await VideoId.GetURLResonite(requestUrl), "text/plain", Encoding.UTF8);
-            return;
-        }
-
-        if (ConfigManager.Config.CacheYouTubeMaxResolution <= 360)
-            avPro = false; // disable browser impersonation when it isn't needed
-
-        // pls no villager
-        if (requestUrl.StartsWith("https://anime.illumination.media"))
-            avPro = true;
-        else if (requestUrl.Contains(".imvrcdn.com") ||
-                 (requestUrl.Contains(".illumination.media") && !requestUrl.StartsWith("https://yt.illumination.media")))
-        {
-            Log.Information("URL Is Illumination media: Bypassing.");
-            await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
-            return;
-        }
-
-        // bypass vfi - cinema 
-        if (requestUrl.StartsWith("https://virtualfilm.institute/"))
-        {
-            Log.Information("URL Is VFI -Cinema: Bypassing.");
-            await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
-            return;
-        }
-
-        var (response, success) = await VideoId.GetUrl(videoInfo, avPro);
-        if (!success)
-        {
-            Log.Error("Get URL: {error}", response);
-            // only send the error back if it's for YouTube, otherwise let it play the request URL normally
-            if (videoInfo.UrlType == UrlType.YouTube)
+            if (string.IsNullOrEmpty(requestUrl))
             {
-                HttpContext.Response.StatusCode = 500;
-                await HttpContext.SendStringAsync(response, "text/plain", Encoding.UTF8);
+                Log.Error("No URL provided.");
+                await HttpContext.SendStringAsync("No URL provided.", "text/plain", Encoding.UTF8);
                 return;
             }
-            response = string.Empty;
-        }
+
+            Log.Information("Request URL: {URL}", requestUrl);
+
+            if (requestUrl.StartsWith("https://dmn.moe"))
+            {
+                requestUrl = requestUrl.Replace("/sr/", "/yt/");
+                Log.Information("YTS URL detected, modified to: {URL}", requestUrl);
+            }
+
+            if (ConfigManager.Config.BlockedUrls.Any(blockedUrl => requestUrl.StartsWith(blockedUrl)))
+            {
+                Log.Warning("URL Is Blocked: {url}", requestUrl);
+                requestUrl = ConfigManager.Config.BlockRedirect;
+            }
+
+            var videoInfo = await VideoId.GetVideoId(requestUrl, avPro);
+            if (videoInfo == null)
+            {
+                Log.Information("Failed to get Video Info for URL: {URL}", requestUrl);
+                return;
+            }
+
+            var (isCached, filePath, fileName) = GetCachedFile(videoInfo.VideoId, avPro);
+            if (isCached)
+            {
+                File.SetLastWriteTimeUtc(filePath, DateTime.UtcNow);
+                CacheManager.AddToCache(fileName);
+                var url = $"{ConfigManager.Config.ytdlWebServerURL}/{fileName}";
+                Log.Information("Responding with Cached URL: {URL}", url);
+                await HttpContext.SendStringAsync(url, "text/plain", Encoding.UTF8);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(videoInfo.VideoId))
+            {
+                Log.Information("Failed to get Video ID: Bypassing.");
+                await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
+                return;
+            }
+
+            if (requestUrl.StartsWith("https://mightygymcdn.nyc3.cdn.digitaloceanspaces.com"))
+            {
+                Log.Information("URL Is Mighty Gym: Bypassing.");
+                await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
+                return;
+            }
+
+            if (source == "resonite")
+            {
+                Log.Information("Request sent from resonite sending json.");
+                await HttpContext.SendStringAsync(await VideoId.GetURLResonite(requestUrl), "text/plain", Encoding.UTF8);
+                return;
+            }
+
+            if (ConfigManager.Config.CacheYouTubeMaxResolution <= 360)
+                avPro = false; // disable browser impersonation when it isn't needed
+
+            // pls no villager
+            if (requestUrl.StartsWith("https://anime.illumination.media"))
+                avPro = true;
+            else if (requestUrl.Contains(".imvrcdn.com") ||
+                     (requestUrl.Contains(".illumination.media") && !requestUrl.StartsWith("https://yt.illumination.media")))
+            {
+                Log.Information("URL Is Illumination media: Bypassing.");
+                await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
+                return;
+            }
+
+            // bypass vfi - cinema 
+            if (requestUrl.StartsWith("https://virtualfilm.institute/"))
+            {
+                Log.Information("URL Is VFI -Cinema: Bypassing.");
+                await HttpContext.SendStringAsync(string.Empty, "text/plain", Encoding.UTF8);
+                return;
+            }
+
+            var (response, success) = await VideoId.GetUrl(videoInfo, avPro);
+            if (!success)
+            {
+                Log.Error("Get URL: {error}", response);
+                // only send the error back if it's for YouTube, otherwise let it play the request URL normally
+                if (videoInfo.UrlType == UrlType.YouTube)
+                {
+                    HttpContext.Response.StatusCode = 500;
+                    await HttpContext.SendStringAsync(response, "text/plain", Encoding.UTF8);
+                    return;
+                }
+                response = string.Empty;
+            }
         
-        Log.Information("Responding with URL: {URL}", response);
-        await HttpContext.SendStringAsync(response, "text/plain", Encoding.UTF8);
-        // check if file is cached again to handle race condition
-        (isCached, _, _) = GetCachedFile(videoInfo.VideoId, avPro);
-        if (!isCached)
-            VideoDownloader.QueueDownload(videoInfo);
+            Log.Information("Responding with URL: {URL}", response);
+            await HttpContext.SendStringAsync(response, "text/plain", Encoding.UTF8);
+            // check if file is cached again to handle race condition
+            (isCached, _, _) = GetCachedFile(videoInfo.VideoId, avPro);
+            if (!isCached)
+                VideoDownloader.QueueDownload(videoInfo);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "GetVideo failed.");
+            HttpContext.Response.StatusCode = 500;
+            await HttpContext.SendStringAsync("Internal server error.", "text/plain", Encoding.UTF8);
+        }
     }
 
     private static (bool isCached, string filePath, string fileName) GetCachedFile(string videoId, bool avPro)
